a3cae66b5fadabf835ad8703778210c278248a65
Jessie Ma <trangmh.sac.ftu@gmail.com>
2026-01-09 23:17:27 -0800

    Update View recipe for FB bit

--- src/pages/OpenRecipePage.js
+++ src/pages/OpenRecipePage.js
@@ -17,7 +17,7 @@ function OpenRecipePage() {
   const getPromptMessage = () => {
     switch(showSignupPrompt) {
       case 'shopping': return 'Open Trackabite on your phone to create a smart shopping list.';
-      case 'cook': return 'Open Trackabite on your phone to log this recipe.';
+      case 'cook': return 'Open this recipe in Trackabite to edit it.';
       case 'servings': return 'Open Trackabite on your phone to adjust servings.';
       default: return 'Open Trackabite on your phone to continue.';
     }
@@ -467,9 +467,10 @@ function OpenRecipePage() {
                   onClick={() => setShowSignupPrompt('cook')}
                 >
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <polyline points="20 6 9 17 4 12"/>
+                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
                   </svg>
-                  Cook this
+                  Edit recipe
                 </button>
               </div>
             </div>
